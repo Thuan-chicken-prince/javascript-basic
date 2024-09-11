e684e7b99e0a2dd087e7e24e70ad9a6a42ee9efe
NguyenDucThuan2005 <thuannd.23it@vku.udn.vn>
2024-09-11 14:02:38 +0700

    Initial commit

--- video6/video6.js
+++
@@ -1,9 +0,0 @@
-console.log("hello world from HTML".toUpperCase());
-let a = 5;
-let b = 6;
-let c = 6.9;
-let d = "5";
-console.log(a + b);
-console.log(a + c);
-console.log(a + d);
-console.log(a === d, "a : ", typeof a, "d :", typeof d);
