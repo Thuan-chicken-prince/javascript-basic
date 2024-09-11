92aaa18ed205a687dfdfe3dbbded6df42db17575
NguyenDucThuan2005 <thuannd.23it@vku.udn.vn>
2024-09-11 15:05:32 +0700

    đây là bài 9

---
+++ video9/video9.js
@@ -0,0 +1,17 @@
+console.log("helllo world from hTML");
+let a = {
+  name: "thuan",
+  age: 25,
+};
+console.log("check data obj :", a);
+console.log(`my name's :`, a.name);
+
+let b = ["MU", "Chelsea", "Liver"];
+console.log("type of b:", typeof b, b);
+
+let family = [
+  { name: "nhu", age: 48 },
+  { name: "hang", age: 20 },
+  { name: "me", age: 18 },
+];
+console.log("My family :", family);
