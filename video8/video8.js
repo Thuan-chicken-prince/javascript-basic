dd7df6b8e9a5340137925b07b221c7d67d78d72e
NguyenDucThuan2005 <thuannd.23it@vku.udn.vn>
2024-09-11 14:41:24 +0700

    đây là bài 8

---
+++ video8/video8.js
@@ -0,0 +1,13 @@
+console.log("hello world from HTML");
+
+//key value
+let obj = {
+  name: "thuan",
+  address: "ha tinh",
+  email: "",
+  gender: "",
+};
+let b = "name";
+obj[b] = "eric";
+console.log("what is your name ?", `my name 's :`, obj.name);
+console.log("what are you from ?", `i'am from  :`, obj.address);
